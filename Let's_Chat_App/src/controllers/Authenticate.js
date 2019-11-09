9f7e838b16d00c980d41c94f99b502696d524d06
ruong32 <trongruong99@gmail.com>
2019-11-09 19:07:06 +0700

    [3][#C1] config view and controller

---
+++ Let's_Chat_App/src/controllers/Authenticate.js
@@ -0,0 +1,7 @@
+let getLoginRegister = (req, res) => {
+    return res.render('login-register/master');
+};
+
+module.exports = {
+    getLoginRegister: getLoginRegister
+};
--- Let's_Chat_App/src/server.js
+++ Let's_Chat_App/src/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import ConnectDB from "./config/ConnectDB";
 import ContactModel from "./models/Contact";
+import initRoutes from './routers/Router';
+import configViewEngine from './config/ConfigView';
 var app = express();
 
 ConnectDB();
 
+configViewEngine(app);
+
+initRoutes(app);
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
     console.log(process.env.APP_PORT);
