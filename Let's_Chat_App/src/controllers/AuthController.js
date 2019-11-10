8b56edce44bda8c501e4695a78fe7561d20b0f65
TuanLamUet <tupikano1@gmail.com>
2019-11-10 17:12:36 +0700

    [6][#A2.1] send mail to confirm register user

--- Let's_Chat_App/src/controllers/AuthController.js
+++ Let's_Chat_App/src/controllers/AuthController.js
@@ -22,7 +22,8 @@ let postRegister = async (req, res) => {
         return res.redirect("/login-register");
     }
     try {
-        let createUserSuccess = await auth.register(req.body.email, req.body.gender, req.body.password);
+        let createUserSuccess = await auth.register(req.body.email, 
+            req.body.gender, req.body.password, req.protocol, req.get("host"));
         successArr.push(createUserSuccess);
         req.flash("success", successArr);
         return res.redirect("/login-register");
@@ -32,8 +33,23 @@ let postRegister = async (req, res) => {
         return res.redirect("/login-register");
     }
 };
+let verifyAccount = async (req, res) => {
+    let errorArr = [];
+    let successArr = [];
+    try {
+        let verifySuccess = await auth.verifyAccount(req.params.token);
+        successArr.push(verifySuccess);
+        req.flash("success", successArr);
+        return res.redirect("/login-register");
+    } catch (error) {
+        errorArr.push(error);
+        req.flash("errors", errorArr);
+        return res.redirect("/login-register");
+    } 
+}
 
 module.exports = {
     getLoginRegister: getLoginRegister,
-    postRegister: postRegister
+    postRegister: postRegister,
+    verifyAccount: verifyAccount
 };
--- Let's_Chat_App/src/services/AuthService.js
+++ Let's_Chat_App/src/services/AuthService.js
@@ -1,21 +1,22 @@
 import UserModel from "./../models/User";
 import bcrypt from 'bcrypt';
 import uuidv4 from "uuid/v4";
-import {errorMessage, successMessage} from "./../../lang/Vie";
+import {errorMessage, successMessage, mailMessage} from "./../../lang/Vie";
+import mailer from "./../config/ConfigMailer";
 
 let salt = 7;
 
-let register =  (email, gender, password) => {
+let register =  (email, gender, password, protocol, host) => {
     return new Promise(async (resolve, reject) => {
         let userByEmail = await UserModel.findByEmail(email);
         if (userByEmail) {
             if (userByEmail.deteleAt != null){
-                return reject(errorMessage.account_removed); 
+                return reject(errorMessage.account_is_removed); 
             }
             if (userByEmail.local.isActive != null){
-                return reject(errorMessage.account_isActive); 
+                return reject(errorMessage.account_is_activated); 
             }
-            return reject(errorMessage.account_in_use);
+            return reject(errorMessage.account_is_used);
         }
         let salt = bcrypt.genSaltSync(salt);
         let userItem = {
@@ -28,13 +29,32 @@ let register =  (email, gender, password) => {
             }
         };
         let user = await UserModel.createNew(userItem);
-        resolve(successMessage.userCreated(user.local.email));
         console.log(user);
+        let linkVerify = `${protocol}://${host}/verify/${user.local.verifyToken}`
+        mailer(email, mailMessage.subject, mailMessage.templace(linkVerify))
+            .then(success => {
+                resolve(successMessage.userCreated(user.local.email));
+            })
+            .catch(async (error) => {
+                await UserModel.removeById(user._id);
+               console.log(error); 
+               reject(mailMessage.send_failed);
+            });
     });
 };
-
+let verifyAccount = (token) => {
+    return new Promise(async (resolve, reject) => {
+        let userByToken = await UserModel.findByToken(token);
+        if (!userByToken) {
+            return reject(errorMessage.account_undefined);
+        }
+        await UserModel.verify(token);
+        resolve(successMessage.account_actived);
+    });
+}
 
 
 module.exports = {
-    register: register 
+    register: register,
+    verifyAccount: verifyAccount 
 }; 
