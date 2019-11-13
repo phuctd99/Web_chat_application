a177c4b85c766f08d3d4740670cbba1facb31fb3
tungvv318 <tungvu3181999@gmail.com>
2019-11-13 15:15:09 +0700

    [11][#A5] fix issue mapping auth controller and router

--- Let's_Chat_App/src/controllers/index.js
+++ Let's_Chat_App/src/controllers/index.js
@@ -3,4 +3,4 @@ import authController from './AuthController';
 import userController from './UserController';
 export const home = homeController;
 export const auth = authController;
-export const user = authController;
+export const user = userController;
